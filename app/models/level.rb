041395d00f6db721aeb6506e072826c0b8764c67
Shusuke Kakiuchi <kakiuchis@gmail.com>
2016-12-30 18:10:23 +0900

    add validation levels_tariff

--- app/models/level.rb
+++ app/models/level.rb
@@ -2,6 +2,7 @@ class Level < ActiveRecord::Base
   belongs_to :field
   belongs_to :levelname
   has_many :projects, dependent: :destroy
-  validates :mintariff, presence: true
-  validates :maxtariff, presence: true
+  validates :mintariff, presence: true, numericality: {greater_than: 0, less_than: :maxtariff, }
+  validates :maxtariff, presence: true, numericality: {greater_than: 0, greater_than: :mintariff, }
+  validates :levelname_id, presence: true
 end
