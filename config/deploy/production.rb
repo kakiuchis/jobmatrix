e1520adf2c4dcfb661e8de9f0d2f21d7f34dafb7
Shusuke Kakiuchi <kakiuchis@gmail.com>
2017-06-25 23:54:26 +0900

    init unicorn app@133-130-66-175

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,5 +60,8 @@
 #     # password: 'please use keys'
 #   }
 
-server '133.130.66.175', user: 'kakiuchi', roles: %w{kakiuchi db web}
-set :ssh_options, keys: '/home/vagrant/.ssh_133.130.66.175/id_rsa'
+# server '133.130.66.175', user: 'kakiuchi', roles: %w{kakiuchi db web}
+# set :ssh_options, keys: '/home/vagrant/.ssh_133.130.66.175/id_rsa'
+
+server '133.130.66.175', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/c/Users/kakiuchi/.ssh/id_rsa'
